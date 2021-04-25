6153448d4c3b42f8facc06d3db2810cf5ec66534
jorgegga <jorgegga.almeria@gmail.com>
2021-04-25 23:57:35 +0200

    prueba cambio

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,12 +1,12 @@
-<nav x-data="{ open: false }" class="border-b border-gray-100 navbar navbar-expand-sm navbar-dark bg-dark" style="background-color: #010648">
+<nav x-data="{ open: false }" class="border-b border-gray-100" style="background-color: #010648">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block fill-current text-gray-600" style="height: 75px;" />
+                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
