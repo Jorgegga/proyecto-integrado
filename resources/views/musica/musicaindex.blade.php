f415805ff4106530065565cda9941f750773f40f
jorgegga <jorgegga.almeria@gmail.com>
2021-04-29 23:00:09 +0200

    audios

--- resources/views/musica/musicaindex.blade.php
+++ resources/views/musica/musicaindex.blade.php
@@ -1,11 +1,11 @@
 @inject('musicMet', 'App\Models\Music');
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-white leading-tight">
             {{ __('Música') }}
         </h2>
     </x-slot>
-
     <x-slot name="cuerpo">
         <section class="row justify-content-center mt-md-4 mb-md-4 mt-sm-4 mb-sm-4">
             <!--<div class="col-10 pr-3">-->
@@ -28,8 +28,10 @@
                                     'nombre' => $musicMet->nomAlbum($item->album_id)[0]->nombre]) }}">{{ $musicMet->nomAlbum($item->album_id)[0]->nombre }}</a>
                             </td>
                             <td>{{ $item->autor }}</td>
-                            <td><audio controls preload="auto">
-                                    <source src="{{ $item->ruta }}" type="audio/mpeg">
+                            <!-- Chrome carga erroneamente los audios, utilizar otro navegador -->
+                            <td><audio controls preload="auto" id="audio">
+                                    <source src="{{asset($item->ruta) }}" type="audio/ogg">
+                                        No lo soporta
                                 </audio>
                             </td>
                         </tr>
