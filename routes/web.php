51bf7cc56a24343f9a00d377c3bb1811255eb840
jorgegga <jorgegga.almeria@gmail.com>
2021-06-06 05:04:15 +0200

    middleware

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,6 @@ Route::resource('generos', GeneroController::class);
 Route::resource('users', UserController::class);
 Route::resource('playlists', PlaylistController::class);
 
-//Creacion de un update aparte
-Route::put('/user/update/{id}',[AreaController::class, 'update'])->name('updateUser');
-
 //Route::get('album/{id}/{nombre}', [AlbumController::class, 'show'])->middleware(['auth', 'verified'])->name('verAlbum');
 Route::get('/album/{album}/{nombre}', [AlbumController::class, 'mostrarAlbum'])->name('verAlbum');
 Route::get('/autor/{autor}/{nombre}', [AutorController::class, 'mostrarAutor'])->name('verAutor');
@@ -45,28 +42,29 @@ Route::get('/autor/pagination', [AutorController::class, 'pagination'])->name('p
 
 Route::get('/album/{id}',[AlbumController::class, 'albumRaw'])->name('albumRaw');
 
-Route::get('/albumAdminUpdate/{id}',[AdminController::class, 'albumRawUpdate'])->name('albumRawUpdate');
-Route::get('/albumAdminDetalles/{id}',[AdminController::class, 'albumRawDetalles'])->name('albumRawDetalles');
-Route::get('/autorAdminUpdate/{id}',[AdminController::class, 'autorRawUpdate'])->name('autorRawUpdate');
-Route::get('/autorAdminDetalles/{id}',[AdminController::class, 'autorRawDetalles'])->name('autorRawDetalles');
-Route::get('/musicAdminUpdate/{id}',[AdminController::class, 'musicRawUpdate'])->name('musicRawUpdate');
-Route::get('/musicAdminDetalles/{id}',[AdminController::class, 'musicRawDetalles'])->name('musicRawDetalles');
-Route::get('/generoAdminUpdate/{id}',[AdminController::class, 'generoRawUpdate'])->name('generoRawUpdate');
-Route::get('/generoAdminDetalles/{id}',[AdminController::class, 'generoRawDetalles'])->name('generoRawDetalles');
-Route::get('/userAdminUpdate/{id}',[AdminController::class, 'userRawUpdate'])->name('userRawUpdate');
-Route::get('/userAdminDetalles/{id}',[AdminController::class, 'userRawDetalles'])->name('userRawDetalles');
+Route::get('/albumAdminUpdate/{id}',[AdminController::class, 'albumRawUpdate'])->middleware('auth')->name('albumRawUpdate');
+Route::get('/albumAdminDetalles/{id}',[AdminController::class, 'albumRawDetalles'])->middleware('auth')->name('albumRawDetalles');
+Route::get('/autorAdminUpdate/{id}',[AdminController::class, 'autorRawUpdate'])->middleware('auth')->name('autorRawUpdate');
+Route::get('/autorAdminDetalles/{id}',[AdminController::class, 'autorRawDetalles'])->middleware('auth')->name('autorRawDetalles');
+Route::get('/musicAdminUpdate/{id}',[AdminController::class, 'musicRawUpdate'])->middleware('auth')->name('musicRawUpdate');
+Route::get('/musicAdminDetalles/{id}',[AdminController::class, 'musicRawDetalles'])->middleware('auth')->name('musicRawDetalles');
+Route::get('/generoAdminUpdate/{id}',[AdminController::class, 'generoRawUpdate'])->middleware('auth')->name('generoRawUpdate');
+Route::get('/generoAdminDetalles/{id}',[AdminController::class, 'generoRawDetalles'])->middleware('auth')->name('generoRawDetalles');
+Route::get('/userAdminUpdate/{id}',[AdminController::class, 'userRawUpdate'])->middleware('auth')->name('userRawUpdate');
+Route::get('/userAdminDetalles/{id}',[AdminController::class, 'userRawDetalles'])->middleware('auth')->name('userRawDetalles');
 
 Route::get('/autocompleteAlbum', [AlbumController::class, 'selectSearch'])->name('albumAuto');
 Route::get('/autocompleteAutor', [AutorController::class, 'selectSearch'])->name('autorAuto');
 
-Route::get('/tablas/album', [AdminController::class, 'album'])->name('adminAlbum');
-Route::get('/tablas/autor', [AdminController::class, 'autor'])->name('adminAutor');
-Route::get('/tablas/music', [AdminController::class, 'musica'])->name('adminMusic');
-Route::get('/tablas/genero', [AdminController::class, 'genero'])->name('adminGenero');
-Route::get('/tablas/user', [AdminController::class, 'user'])->name('adminUser');
+Route::get('/tablas/album', [AdminController::class, 'album'])->middleware('auth')->name('adminAlbum');
+Route::get('/tablas/autor', [AdminController::class, 'autor'])->middleware('auth')->name('adminAutor');
+Route::get('/tablas/music', [AdminController::class, 'musica'])->middleware('auth')->name('adminMusic');
+Route::get('/tablas/genero', [AdminController::class, 'genero'])->middleware('auth')->name('adminGenero');
+Route::get('/tablas/user', [AdminController::class, 'user'])->middleware('auth')->name('adminUser');
 
-Route::get('/user/{id}/{user}/playlist', [AreaController::class, 'playlist'])->name('playlistUser');
-Route::get('/user/{id}/{user}/area', [AreaController::class, 'inicioUser'])->name('areaUser');
+Route::put('/user/update/{id}',[AreaController::class, 'update'])->middleware('auth')->name('updateUser');
+Route::get('/user/{id}/{user}/playlist', [AreaController::class, 'playlist'])->middleware('auth')->name('playlistUser');
+Route::get('/user/{id}/{user}/area', [AreaController::class, 'inicioUser'])->middleware('auth')->name('areaUser');
 
 //Route formularios correos
 Route::post('/formNegocios', [ContactoController::class, 'formNegocios'])->name('formNegocios');
