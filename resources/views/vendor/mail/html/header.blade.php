0995ef2f89e910cb1b4f40c13bc5673133d006e0
jorgegga <jorgegga.almeria@gmail.com>
2021-06-09 20:50:11 +0200

    imagen correo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{route('inicios.index')}}" height="50" width="200">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Scarlet Perception')
 <img src="https://i.postimg.cc/Y9pdhC1p/logo.png">
 @else
 {{ $slot }}
